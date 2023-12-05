c936a91df59d00c08a1525889ff7583404e9bded
Ayskin <yann.ennassih@epfl.ch>
2023-12-05 15:22:40 +0100

    Adjust model_handler

--- helpers/model_handler.py
+++ helpers/model_handler.py
@@ -2,42 +2,40 @@ import os
 import torch
 from datetime import datetime
 
-MODEL_SAVE_PATH = '../model/'
-
-def save_model(model):
+def save_model(model, path):
     """
     Saves the model to the MODEL_SAVE_PATH directory with the current date and time
 
     Args:
         model: Model to save
+        path: Parent path of the model
 
     Returns:
         None
     """
 
-    if not os.path.isdir(MODEL_SAVE_PATH):
-        os.mkdir(MODEL_SAVE_PATH)
+    if not os.path.isdir(path):
+        os.mkdir(path)
     date = datetime.now().strftime("%Y-%m-%d")
     filename = f"model_{date}_"
     i = 0
-    while os.path.exists(MODEL_SAVE_PATH + filename + str(i) + ".pt"):
+    while os.path.exists(path + filename + str(i) + ".pt"):
         i += 1
 
-    torch.save(model, MODEL_SAVE_PATH + filename + str(i) + ".pt")
+    torch.save(model, path + filename + str(i) + ".pt")
 
-    print("Model saved to " + MODEL_SAVE_PATH + filename + str(i) + ".pt")
+    print("Model saved to " + path + filename + str(i) + ".pt")
 
-def load_model(name):
+def load_model(path):
     """
     Load previously saved model. Warning: loads entire architecture and structure of the model
 
     Args:
-        name: file name containing the model
+        path: file path containing the model
     
     Returns:
         the model
     """
-    path = MODEL_SAVE_PATH + name
     assert os.path.exists(path), f"Unable to find {path}"
     
     return torch.load(path)
