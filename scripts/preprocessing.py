300e55edd05bb172da2344a9709c59070412320b
Ayskin <yann.ennassih@epfl.ch>
2023-12-15 17:43:18 +0100

    Add load and concatenate img fctions

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -194,3 +194,24 @@ def split_data(x, y, ratio, seed=None):
     f"and TEST {x_te.shape} & {y_te.shape}")
 
     return x_tr, x_te, y_tr, y_te
+
+def load_image(infilename):
+    data = mpimg.imread(infilename)
+    return data
+
+# Concatenate an image and its groundtruth
+def concatenate_images(img, gt_img):
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)
+        gt_img_3c[:, :, 0] = gt_img8
+        gt_img_3c[:, :, 1] = gt_img8
+        gt_img_3c[:, :, 2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
